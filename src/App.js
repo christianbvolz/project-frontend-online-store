08d7fc7250ac86edb589efd02e05211e85702cc1
João Ferreira <joaovtf39@gmail.com>
2021-09-17 15:41:32 -0300

    apenas um teste de como funciona

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - GRUPO 14 GO GO
         </a>
       </header>
     </div>
