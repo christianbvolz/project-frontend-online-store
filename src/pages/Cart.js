f44745fcb70c2f05e1c3ba0959bd0fdba0e773a2
Christian Volz <christianbvolz@gmail.com>
2021-09-22 15:04:04 -0300

    requisito 8

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react';
 
 export default class Cart extends Component {
   render() {
+    const products = JSON.parse(localStorage.getItem('cart'));
     return (
       <div>
+        {products.map(({ title, price, thumbnail, id, quantity }) => (
+          <div
+            key={ id }
+            data-testid="product"
+          >
+            <p data-testid="shopping-cart-product-name">{ title }</p>
+            <img src={ thumbnail } alt={ title } />
+            <p>{ price }</p>
+            <p data-testid="shopping-cart-product-quantity">{ quantity }</p>
+          </div>
+        ))}
         <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
       </div>
     );
